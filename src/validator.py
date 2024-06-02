4a33f400ee46a02fc2b86bc41c039e0f5daf2072
PajakaL <60051149+PajakaL@users.noreply.github.com>
2024-06-01 23:45:38 -0700

    added leave_one_out

--- src/validator.py
+++ src/validator.py
@@ -5,3 +5,16 @@ class Validator:
         self.feature_subset = None
         self.classifier = classifier 
         self.df = df
+
+    def leave_one_out(self):
+        score = 0
+        for i in range(len(self.df)):
+            training = self.df[self.df.index != i]
+            training_instance = self.df.iloc[i]
+            self.classifier.train(training)
+            predict = self.classifier.test(test_instance[self.feature_subset].values)
+            if predict == training_instance.iloc[0]:
+                score = score + 1
+        accuracy = score / len(self.df)
+        return accuracy
+        
