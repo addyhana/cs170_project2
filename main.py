70a24d1fd043a65406bcd0db159ce57831daa6af
Anisha Siva <anishasiva@Anishas-Laptop.local>
2024-06-09 00:39:43 -0700

    finished menu

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ import src.util
 from src.NNclassifier import NNclassifier
 from src.search import greedyforward, greedybackward
 
-# src.util.menu()
+src.util.menu()
 
-filename = "small-test-dataset.txt"
-df = src.util.smallDfLoader(filename)
-print("with greedy forward:")
-selected_features, max_accuracies = greedyforward(df)
-testingWSearch = NNclassifier(features=selected_features)
-accuracy = testingWSearch.validate(df=df)
-print(accuracy)
+#filename = "small-test-dataset.txt"
+#df = src.util.smallDfLoader(filename)
+#print("with greedy forward:")
+#selected_features, max_accuracies = greedyforward(df)
+#testingWSearch = NNclassifier(features=selected_features)
+#accuracy = testingWSearch.validate(df=df)
+#print(accuracy)
 
-print("vs. with greedy backward:")
-selected_features, max_accuracies, removed_features = greedybackward(df)
-testingWSearch = NNclassifier(features=selected_features)
-print(f"{selected_features}")
-accuracy = testingWSearch.validate(df=df)
-print(accuracy)
+#print("vs. with greedy backward:")
+#selected_features, max_accuracies, removed_features = greedybackward(df)
+#testingWSearch = NNclassifier(features=selected_features)
+#print(f"{selected_features}")
+#accuracy = testingWSearch.validate(df=df)
+#print(accuracy)
 
-print("vs given without search:")
-testingNoSearch = NNclassifier(features=[3,5,7])
-accuracy = testingNoSearch.validate(df=df)
-print(accuracy)
+#print("vs given without search:")
+#testingNoSearch = NNclassifier(features=[3,5,7])
+#accuracy = testingNoSearch.validate(df=df)
+#print(accuracy)
 
--- src/util.py
+++ src/util.py
@@ -1,16 +1,57 @@
 
 import pandas as pd
+from src.NNclassifier import NNclassifier
+import src.search
+from src.search import greedyforward, greedybackward
 
 def menu():
-    print("Welcome to Group 63's Feature Selection Project")
-    featureNum = input("Please enter total number of features: ")
+    print("Welcome to Group 63's Feature Selection Algorithm")
+    file_to_test = input("Type in the name of the file to test: ")
     print("Type the number of the algorithm you want to run")
     print("1. Forward Selection")
     print("2. Backward Elimination")
-    print("3. Group 63's Special Algorithm ")
-    featureNum = input("Enter number: ")
+    selected_algo = input("Enter number: ")
 
-def smallDfLoader(filename):
+    if file_to_test == 'small-test-dataset.txt':
+        df = smallDfLoader()
+
+    elif file_to_test == 'CS170_Spring_2024_Small_data__62.txt':
+        df = smallDfCustomLoader()
+
+    elif file_to_test == 'large-test-dataset.txt':
+        df = largeDfLoader()
+
+    elif file_to_test == 'CS170_Spring_2024_Large_data__62.txt':
+        df = largeDfCustomLoader()
+
+    print()
+
+    print(f"This dataset has {len(df.columns) - 1} features (not including the class attribute), with {len(df)} instances.\n")
+    print("Please wait while I normalize the data...")
+
+    normalized_df = normalize_vals(df)
+
+    
+
+    if selected_algo == '1':
+        selected_features, max_accuracies = greedyforward(normalized_df)
+        outputSearch = NNclassifier(features=selected_features)
+        
+
+    if selected_algo == '2':
+        selected_features, max_accuracies, removed_features = greedybackward(df)
+        outputSearch = NNclassifier(features=selected_features)
+        
+        
+
+
+
+
+
+
+    
+
+def smallDfLoader():
     labels = []
     labels.insert(0, "Feature")
     for i in range(1, 11):
@@ -26,6 +67,22 @@ def largeDfLoader():
     for i in range(1, 41):
         col_names.append(i)
     df = pd.read_csv("large-test-dataset.txt", sep='\s+', names=col_names)
+    return df
+
+def smallDfCustomLoader():
+    col_names = []
+    col_names.insert(0, "Label")
+    for i in range(1, 11):
+        col_names.append(i)
+    df = pd.read_csv("CS170_Spring_2024_Small_data__62.txt", sep='\s+', names=col_names)
+    return df
+
+def largeDfCustomLoader():
+    col_names = []
+    col_names.insert(0, "Label")
+    for i in range(1, 41):
+        col_names.append(i)
+    df = pd.read_csv("CS170_Spring_2024_Large_data__62.txt", sep='\s+', names=col_names)
     return df
 
 def normalize_vals(df):
